a6ae17e6804fb99a79b96a049d12972574733b73
Davi <davi_pexoto@hotmail.com>
2020-02-04 02:10:56 -0300

    WIP: Offline implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage';
+import { Network } from '@ionic-native/network/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Network
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,132 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  rates = {};
+  ratesKeys = [];
+  date = "";
+  base = "";
+  count = 0;
+  formatedTime = "";
+  fromCurrency = "";
+  toCurrency = "";
+  covertedValue = 0;
+  amount = 0;
 
-  constructor() {}
+  constructor(private http: HttpClient, private storage: Storage) {
+    this.fecthData()
 
+    setInterval(() => {
+      this.count++;
+      this.formatTime(); 
+      }, 1000);
+  }
+  
+  public changeBase() {
+    console.log(this.base);
+    // this.base = param;
+    this.fecthData();
+  }
+
+  public formatTime() {
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let rest = 0;
+    let formatedTime = "";
+
+    if(this.count >= 360)  {
+      hours = this.count / 60;
+      rest = this.count - (60 * hours);
+      formatedTime = hours.toString();
+      formatedTime += " horas";
+    }
+    
+    if(rest >= 60) {
+      if(hours > 0) {
+        formatedTime += ", ";
+      }
+      minutes = this.count / 60;
+      rest = this.count - (60 * hours);
+      formatedTime = minutes.toString();
+      formatedTime += " minutos"
+    }
+
+    if(hours > 0 || minutes > 0) {
+      formatedTime += " e ";
+      formatedTime = rest.toString();      
+    }
+
+    else {
+      formatedTime = this.count.toString();
+    }
+
+    formatedTime += " segundos";
+    this.formatedTime = formatedTime;
+  }
+
+  public convertCurrency() {
+    let amount = this.amount;
+    let eurAmount = 0;
+    let convertedAmount = 0;
+    let fromCurrency = this.fromCurrency.trim();
+    let toCurrency = this.toCurrency.trim();
+    let rates = this.rates;
+    
+    console.log(rates)
+    console.log(fromCurrency)
+    console.log(toCurrency)
+    console.log(rates[fromCurrency])
+    console.log(rates[toCurrency])
+
+    if(rates[fromCurrency] > 1) {
+      eurAmount = amount / rates[fromCurrency];
+    }
+
+    else {
+      eurAmount = amount * rates[fromCurrency];
+    }
+
+    console.log(eurAmount)
+
+    if(rates[toCurrency] > 1) {
+      convertedAmount = eurAmount * rates[toCurrency];
+    }
+
+    else {
+      convertedAmount = eurAmount / rates[toCurrency];
+    }
+
+    console.log(convertedAmount);
+    this.covertedValue = convertedAmount;
+  }
+
+  test() {
+    this.storage.get('rates')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  fecthData() {
+    this.http.get(`https://api.exchangeratesapi.io/latest`).subscribe({
+        next: data => {
+                        console.log(data)
+                        this.count = 0;
+                        this.rates = data["rates"];
+                        this.date = data["date"];
+                        this.ratesKeys = Object.keys(data["rates"]);
+                        this.storage.set('rates', data["rates"])
+                        .then(res => {
+                          console.log(res);
+                        })
+                        .catch(err => {
+                          console.log(err);
+                        })
+                      },
+        error: err => console.log(err)
+      });
+  }
 }
